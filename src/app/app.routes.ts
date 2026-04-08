69a54598704146936d94f21d2a35efe3725cde6f
Sokmontrey <sythatsokmontrey@gmail.com>
2026-04-08 10:34:57 -0400

    app and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { CharacterListComponent } from './components/characterlist/characterlist.component';
+import { CharacterDetailsComponent } from './components/characterdetails/characterdetails.component';
+import { CharacterFilterComponent } from './components/characterfilter/characterfilter.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'characters', pathMatch: 'full' },
+  { path: 'characters', component: CharacterListComponent },
+  { path: 'characters/:id', component: CharacterDetailsComponent },
+  { path: 'filter', component: CharacterFilterComponent },
+];
